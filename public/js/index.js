89fbe0f6b135b3b6e76066b1d12e5ebf33c532bd
A.T. Sidharth <atsidharth@gmail.com>
2021-02-25 20:08:05 +0530

    =test

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,6 @@ const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg-1')
 const msg2 = document.querySelector('#msg-2')
 
-
-
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
@@ -21,7 +19,7 @@ weatherForm.addEventListener('submit',(e)=>{
                 msg1.textContent = data.error
             }else{
                 msg1.textContent = data.location
-                msg2.textContent = data.forecastData
+                msg2.textContent = data.forecastData 
             }
             
         })
